ba5115ea8727e6302c3b8dc69d9aa6bde41b90be
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-01-30 19:02:58 -0600

    MVP Met

--- user-onboarding-app/src/components/UserForm.js
+++ user-onboarding-app/src/components/UserForm.js
@@ -1,13 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
-import UsersRendered from './UsersRendered';
 // UsersRendered users={users}
 
 function UserForm({values, errors, touched, status}) {
 
   const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+
+    status && setUsers(users => [...users, status]);
+  }, [status]);
 
   return(
     <div className="form-field">
@@ -20,12 +24,16 @@ function UserForm({values, errors, touched, status}) {
             name="name"
             placeholder="Enter Name"
           />
+          {touched.name && errors.name && (
+            <p>{errors.name}</p>
+          )}
         </label>
         {/* EMAIL */}
         <label>
           Email Address:
           <Field
             type="email"
+            id="email"
             name="email"
             placeholder="Enter Email"
           />
@@ -50,7 +58,14 @@ function UserForm({values, errors, touched, status}) {
         {/* SUBMIT BUTTON */}
         <button type="submit">SUBMIT</button>
       </Form>
-      <UsersRendered />
+      {users.map(user => {
+        return(
+          <ul key={user.id}>
+            <li>Name: {user.name}</li>
+            <li>Email: {user.email}</li>
+          </ul>
+        );
+      })}
     </div>
   );
 };
@@ -58,9 +73,28 @@ function UserForm({values, errors, touched, status}) {
 const FormikUserForm = withFormik({
   mapPropsToValues(props) {
     return {
-      
+      name: props.name || '',
+      email: props.email || '',
+      password: props.password || ''
     }
-  }
+  },
+
+  handleSubmit(values, { setStatus, resetForm }) {
+    console.log("submitting", values);
+    axios
+      .post("https://reqres.in/api/users/", values)
+      .then(res => {
+        console.log("success", res);
+        setStatus(res.data);
+        resetForm();
+      })
+      .catch(err => console.log(err.response));
+  },
+
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required(),
+    email: Yup.string().required()
+  })
 })(UserForm);
 
 export default FormikUserForm;
